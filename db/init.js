f0bfdaa580b0140816a2ab157203a8c6536d2dc1
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-07-13 12:56:16 +1200

    Add more users tweets

--- db/init.js
+++ db/init.js
@@ -16,10 +16,20 @@ const users = [
 ];
 
 const tweets = [
-  {tweet: "Life is good!", date: "2020-05-25 23:59", user_id: 1,},
-  {tweet: "I bought a boat!", date: "2021-06-26 14:34", user_id: 3,},
-  {tweet: "Hmmm... It's something...", date: "2019-01-02 17:23", user_id: 4,},
-  {tweet: "Well, I have a cat <3", date: "2021-11-10 11:34", user_id: 2,},
+  {tweet: "Life is good!", date: "2020-05-25T23:59:00+12:00", user_id: 1,},
+  {tweet: "I bought a boat!", date: "2021-06-26T14:34:15+12:00", user_id: 3,},
+  {tweet: "Hmmm... It's something...", date: "2019-01-02T17:23:34+12:00", user_id: 4,},
+  {tweet: "Well, I have a cat <3", date: "2021-11-10T11:34:07+12:00", user_id: 2,},
+  {tweet: "No one is born hating another person because of the color of his skin or his background or his religion...", date: "2005-04-17T08:22:55+12:00", user_id: 2,},
+  {tweet: "Hey guys, wanna feel old? I'm 40. You're welcome.", date: "1999-12-23T16:40:23+12:00", user_id: 1,},
+  {tweet: "Happy Birthday To Me!!!", date: "2011-09-07T20:23:11+12:00", user_id: 4,},
+  {tweet: "Very creative!", date: "2001-07-27T01:22:34+12:00", user_id: 3,},
+  {tweet: "🙃", date: "2007-12-11T23:03:22+12:00", user_id: 4,},
+  {tweet: "Happy Birthday Ada!", date: "2011-05-02T12:15:01+12:00", user_id: 1,},
+  {tweet: "Don't drink and Tweet", date: "1995-11-02T12:04:33+12:00", user_id: 3},
+  {tweet: "This actually just happened. This is not fake. Click the link to confirm for yourself.", date: "2008-10-27T19:05:03+12:00", user_id: 2,},
+  {tweet: "Love you 😘", date: "2021:01:01T01:46:22+12:00", user_id: 3,},
+  {tweet: "WHAT’S HAPPENING!?", date: "1999-02-26T18:22:33+12:00", user_id: 2,},
 ];
 
 const followers = [
