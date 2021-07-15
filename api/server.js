7ae27c3ab47863a2c61dbae751c7369985a1ffc3
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-07-15 22:39:34 +1200

    Implement tweets retrieval for curent user

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ const tweetsRoutes = require("./tweets");
 const authRoutes = require("./auth");
 
 server.use("/api/v1/users", tweetsRoutes);
+server.use("/api/v1/me", tweetsRoutes);
 server.use("/api/v1", authRoutes);
 
 module.exports = server;
--- api/tweets.js
+++ api/tweets.js
@@ -6,12 +6,28 @@ const router = express.Router();
 const file = __dirname + "/../db/db.sqlite";
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(file);
+const jwt = require("express-jwt");
+const SECRET_KEY = 'mysecretkey12345';
 
 router.get("/:userName/tweets", (req, res) => {
   const name = req.params.userName;
   const sql = 'SELECT tweet, date FROM tweets WHERE user_id = (SELECT id FROM users WHERE lower(name) = ?) ORDER BY date DESC';
 
   db.all(sql, [name], (err, rows) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.json(rows);
+  });
+});
+
+router.get("/tweets", jwt({secret: SECRET_KEY, algorithms: ['HS256'], credentialsRequired: false,},), (req, res) => {
+  const userId = req.user.user_id;
+  const sql = 'SELECT tweet, date FROM tweets WHERE user_id = ? ORDER BY date DESC';
+
+  db.all(sql, [userId], (err, rows) => {
     if (err) {
       console.log(err);
       return;
