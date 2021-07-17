e6bb7f9bed0d1235946a1684450136cb694d8fbb
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-07-17 12:49:40 +1200

    Implement tweet deletion

--- api/tweets.js
+++ api/tweets.js
@@ -7,9 +7,10 @@ const file = __dirname + "/../db/db.sqlite";
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(file);
 const jwt = require("express-jwt");
+const server = require("./server");
 const SECRET_KEY = 'mysecretkey12345';
 
-router.get("/tweets", jwt({secret: SECRET_KEY, algorithms: ['HS256'], credentialsRequired: false,},), (req, res) => {
+router.get("/tweets", jwt({secret: SECRET_KEY, algorithms: ['HS256']},), (req, res) => {
   const userId = req.user.user_id;
   const sql = 'SELECT tweet, date FROM tweets WHERE user_id = ? ORDER BY date DESC';
 
@@ -23,7 +24,7 @@ router.get("/tweets", jwt({secret: SECRET_KEY, algorithms: ['HS256'], credential
   });
 });
 
-router.post("/tweets", jwt({secret: SECRET_KEY, algorithms: ['HS256'], credentialsRequired: false,},), (req, res) => {
+router.post("/tweets", jwt({secret: SECRET_KEY, algorithms: ['HS256']},), (req, res) => {
   const myTweet = req.body.tweet;
   const userId = req.user.user_id;
   const sql = "INSERT INTO tweets (tweet, date, user_id) VALUES (?, datetime('now') || 'Z', ?)";
@@ -44,6 +45,37 @@ router.post("/tweets", jwt({secret: SECRET_KEY, algorithms: ['HS256'], credentia
        }
 
       res.json(row);
+    });
+  });
+});
+
+router.delete("/tweets/:id", jwt({secret: SECRET_KEY, algorithms: ['HS256']},), (req, res) => {
+  const tweetId = req.params.id;
+  const userId = req.user.user_id;
+  const sql = 'SELECT user_id FROM tweets where id = ?';
+
+  db.get(sql, [tweetId], (err, row) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else if (row === undefined) {
+      res.status(404).send("No tweet. Sorry, can't find that!");
+      return;
+    }
+
+    if (row.user_id !== userId) {
+      res.status(403).end();
+      return;
+    }
+
+    const tweetToDelete = 'DELETE FROM tweets WHERE id = ?';
+
+    db.run(tweetToDelete, [tweetId], (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.status(200).json("Tweet was deleted.");
     });
   });
 });
